adccd2cf913c79ae8717c8fa8f2ff6703e836a67
daviddalbusco <david.dalbusco@outlook.com>
2018-10-13 09:17:42 +0200

    Import

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+    image: SafeResourceUrl;
+
+    constructor(private sanitizer: DomSanitizer) {
+    }
+
+    async takePicture() {
+        const { Camera } = Plugins;
+
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: CameraResultType.Base64,
+            source: CameraSource.Camera
+        });
+
+        // Example of using the Base64 return type. It's recommended to use CameraResultType.Uri
+        // instead for performance reasons when showing large, or a large amount of images.
+        this.image = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.base64Data));
+    }
+
+}
